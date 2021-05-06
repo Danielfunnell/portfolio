eaddec6aaed1de7d4328e163511607f67349693c
Daniel <danielfunnell@yahoo.co.uk>
2021-05-06 20:48:46 +0100

    updated projects section

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,8 +30,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'premier-drive-snippet.png',
     title: 'Premier Drive',
-    info: '',
-    info2: '',
+    info: 'A custom-themed WordPress website for my driving school. It includes Woocommerce, Advanced custom fields plugins and custom post types for my testimonials section.',
+    info2: 'I have also created a JavaScript driven questionnaire to allow customers to find the most suitable intensive driving course for themselves..',
     url: 'https://premierdrive.co.uk/',
     repo: 'https://github.com/Danielfunnell/premierdrive', // if no repo, the button will not show up
     tagOne: 'Wordpress',
@@ -39,18 +39,25 @@ export const projectsData = [
     tagThree: 'Woocommerce',
     tagFour: 'Php',
     tagFive: 'Bootstrap',
-    tagSix: 'Javascript',
+    tagSix: 'JavaScript',
     tagSevern: 'Sass',
     tagEight: 'Laravel-Mix'
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'ontrack-snippet.png',
+    title: 'On-track db',
+    info: 'A full-stack application. Allows instructors to log in and update their students progress and next lesson details. Gives students the ability to securely log in and view their profile and driving progress.',
+    info2: 'Features include a message centre for instructors and students to message each other. I have also created a forgotten password system with emailed tokens to reset.',
+    url: 'https://www.ontrackdb.co.uk/',
+    repo: 'https://github.com/Danielfunnell/ontrackdb', // if no repo, the button will not show up
+    tagOne: 'Php',
+    tagTwo: 'MySQL',
+    tagThree: 'Bootstrap',
+    tagFour: 'Sass',
+    tagFive: 'JavaScript',
+    tagSix: 'Ajax',
+    tagSevern: 'Laravel-Mix',
   },
   {
     id: nanoid(),
@@ -91,7 +98,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Danielfunnell',
     },
   ],
 };
