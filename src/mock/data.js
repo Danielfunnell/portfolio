9435f0ad6c15d4f3371c278698989648814b1409
Daniel <danielfunnell@yahoo.co.uk>
2021-05-17 22:42:42 +0100

    updated projects section

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Daniel Funnell', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -30,8 +30,10 @@ export const projectsData = [
     id: nanoid(),
     img: 'premier-drive-snippet.png',
     title: 'Premier Drive',
-    info: 'A custom-themed WordPress website for my driving school. It includes Woocommerce, Advanced custom fields plugins and custom post types for my testimonials section.',
-    info2: 'I have also created a JavaScript driven questionnaire to allow customers to find the most suitable intensive driving course for themselves..',
+    info:
+      'A custom-themed WordPress website for my driving school. It includes Woocommerce, Advanced custom fields plugins and custom post types for my testimonials section.',
+    info2:
+      'I have also created a JavaScript driven questionnaire to allow customers to find the most suitable intensive driving course for themselves..',
     url: 'https://premierdrive.co.uk/',
     repo: 'https://github.com/Danielfunnell/premierdrive', // if no repo, the button will not show up
     tagOne: 'Wordpress',
@@ -41,14 +43,16 @@ export const projectsData = [
     tagFive: 'Bootstrap',
     tagSix: 'JavaScript',
     tagSevern: 'Sass',
-    tagEight: 'Laravel-Mix'
+    tagEight: 'Laravel-Mix',
   },
   {
     id: nanoid(),
     img: 'ontrack-snippet.png',
     title: 'On-track db',
-    info: 'A full-stack application. Allows instructors to log in and update their students progress and next lesson details. Gives students the ability to securely log in and view their profile and driving progress.',
-    info2: 'Features include a message centre for instructors and students to message each other. I have also created a forgotten password system with emailed tokens to reset.',
+    info:
+      'A full-stack application. Allows instructors to log in and update their students progress and next lesson details. Gives students the ability to securely log in and view their profile and driving progress.',
+    info2:
+      'Features include a message centre for instructors and students to message each other. I have also created a forgotten password system with emailed tokens to reset.',
     url: 'https://www.ontrackdb.co.uk/',
     repo: 'https://github.com/Danielfunnell/ontrackdb', // if no repo, the button will not show up
     tagOne: 'Php',
@@ -58,15 +62,26 @@ export const projectsData = [
     tagFive: 'JavaScript',
     tagSix: 'Ajax',
     tagSevern: 'Laravel-Mix',
+    tagEight: '',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'image-outlet-snippet',
+    title: 'Image Outlet',
+    info:
+      'My React application allowing users to search for images. This project fetches images using the Pixabay API',
+    info2:
+      'Users can click on indiviual images to get more details such as likes, downloads and creater.',
+    url: 'https://imageoutlet.netlify.app/',
+    repo: 'https://github.com/Danielfunnell/image-outlet', // if no repo, the button will not show up
+    tagOne: 'React',
+    tagTwo: 'React Router',
+    tagThree: 'Material Ui',
+    tagFour: 'Javascript',
+    tagFive: 'API',
+    tagSix: '',
+    tagSevern: '',
+    tagEight: '',
   },
 ];
 
@@ -74,7 +89,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'danielfunnell@yahoo.co.uk',
 };
 
 // FOOTER DATA
